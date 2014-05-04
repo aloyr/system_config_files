45447763d9ff2dc6551c821f9439234776878472
Peter Carrero <peter.carrero@gmail.com>
2014-05-04 11:12:36 -0500

    Add gitless set_prompt.sh

---
+++ set_prompt.sh
@@ -0,0 +1,15 @@
+# some more ls aliases
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+
+ # Set prompt
+TTYNAME=`tty|cut -b 6-`
+USUARIO=`id -u`
+if [ $USUARIO -eq 0 ]; then
+  PS1="# \[\033[1;31m\]\u@\h\[\033[01;34m\]($TTYNAME) \[\033[1;34m\]\w\[\033[0m\]\n"
+else
+  PS1="# \[\e[32m\]\u@\h\[\033[01;34m\]($TTYNAME) \[\033[1;34m\]\w\[\033[0m\]\n"
+fi
+
+echo "done"
