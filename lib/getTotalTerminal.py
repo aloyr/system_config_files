331856629f54570bac5a4575793dcfe5b7cd4533
Peter Carrero <peter.carrero@gmail.com>
2014-07-20 22:38:26 -0500

    lib: Remove ref to totalfinder from totalterminal

--- lib/getTotalTerminal.py
+++ lib/getTotalTerminal.py
@@ -6,4 +6,4 @@ def urlimport(url):
 
 urlimport('https://raw.githubusercontent.com/aloyr/system_config_files/master/lib/autoinstall.py')
 
-download('http://totalfinder.binaryage.com/')
+download('http://totalterminal.binaryage.com/')
