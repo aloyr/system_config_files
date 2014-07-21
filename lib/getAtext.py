03a07688f6efc1c0f8a61893b3bdbb2a79d7975a
Peter Carrero <peter.carrero@gmail.com>
2014-07-20 21:52:21 -0500

    lib: Test import lib from url

--- lib/getAtext.py
+++ lib/getAtext.py
@@ -1,3 +1,7 @@
-import autoinstall
+import urllib
+def urlimport(x):
+      exec urllib.urlopen(x) in globals()
+
+urlimport('https://raw.githubusercontent.com/aloyr/system_config_files/master/lib/autoinstall.py')
 
 autoinstall.download('http://www.trankynam.com/atext/')
