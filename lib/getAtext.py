2a37c464e5d1ffcd5f8cd1c07354acd642e32b6d
Peter Carrero <peter.carrero@gmail.com>
2014-07-20 22:23:03 -0500

    lib: Try another way to import module from url

--- lib/getAtext.py
+++ lib/getAtext.py
@@ -1,10 +1,11 @@
 import urllib
 def urlimport(url):
-  filepath = urllib.urlretrieve(url)[0]
-  with open(filepath) as f:
-    exec f in globals();
-  #exec urllib.urlopen(x) in globals()
+  #filepath = urllib.urlretrieve(url)[0]
+  #with open(filepath) as f:
+  #  exec f in globals();
+  exec urllib.urlopen(x) in globals()
 
 urlimport('https://raw.githubusercontent.com/aloyr/system_config_files/master/lib/autoinstall.py')
 
-autoinstall.download('http://www.trankynam.com/atext/')
+#autoinstall.download('http://www.trankynam.com/atext/')
+download('http://www.trankynam.com/atext/')
