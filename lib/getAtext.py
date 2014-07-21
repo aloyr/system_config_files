60a2c2f34ee669ab77cf5538a7b62b6bb52d94ad
Peter Carrero <peter.carrero@gmail.com>
2014-07-20 22:13:57 -0500

    lib: Test import moduel from url...

--- lib/getAtext.py
+++ lib/getAtext.py
@@ -1,6 +1,9 @@
 import urllib
 def urlimport(x):
-  exec urllib.urlopen(x) in globals()
+  filepath = urllib.urlretrieve(url)[0]
+  with open(filepath) as f:
+    exec f in globals();
+  #exec urllib.urlopen(x) in globals()
 
 urlimport('https://raw.githubusercontent.com/aloyr/system_config_files/master/lib/autoinstall.py')
 
