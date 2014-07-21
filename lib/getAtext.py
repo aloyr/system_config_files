af83322767889a69cccfc535cbe69d428cc9b142
Peter Carrero <peter.carrero@gmail.com>
2014-07-20 22:25:20 -0500

    lib: Fix typo in autoinstall

--- lib/getAtext.py
+++ lib/getAtext.py
@@ -3,7 +3,7 @@ def urlimport(url):
   #filepath = urllib.urlretrieve(url)[0]
   #with open(filepath) as f:
   #  exec f in globals();
-  exec urllib.urlopen(x) in globals()
+  exec urllib.urlopen(url) in globals()
 
 urlimport('https://raw.githubusercontent.com/aloyr/system_config_files/master/lib/autoinstall.py')
 
