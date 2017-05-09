4440a94003e93c9eb48455e458be9efb559a58a0
Peter Carrero <peter.carrero@gmail.com>
2017-05-09 01:02:30 -0500

    Downlod drush complete

--- setup.bash
+++ setup.bash
@@ -235,7 +235,7 @@ for gitFile in git-completion.bash git-prompt.sh; do
   getGitFile $gitFile
 done
 
-for aloyrFile in gitadd.bash gitpush.bash set_prompt.bash geoip; do
+for aloyrFile in gitadd.bash gitpush.bash set_prompt.bash geoip drush.complete; do
   getAloyrFile $aloyrFile
 done
 
